efeec5d6df7d5a07829817ed30fa9c670af36c02
Arda <93056630+Ardaa82@users.noreply.github.com>
2024-04-15 20:28:06 +0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,14 +4,15 @@ import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHlpL71Yeljb4_dTHR3gBqcOUmPGUd_Y8",
-  authDomain: "ardaa-tr.firebaseapp.com",
-  databaseURL: "https://ardaa-tr-default-rtdb.firebaseio.com",
-  projectId: "ardaa-tr",
-  storageBucket: "ardaa-tr.appspot.com",
-  messagingSenderId: "568571837732",
-  appId: "1:568571837732:web:edc13e72fc18613890c7b1",
-  measurementId: "G-TQM2CVYLJE"
+  //Buralara firebase den aldığınız database bilgileri gelecek!!
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
